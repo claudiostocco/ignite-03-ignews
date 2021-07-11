69ed377beb48fdf186db3fb3627fd94c697dcb6e
claudiostocco <claudiostocco@gmail.com>
2021-07-11 08:39:25 -0300

    Connecting with FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,5 +11,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user'
     }),
-  ]
+  ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user,account,profile) {
+      const { name, email } = user
+      try {
+        console.log(user)
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'),q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'),
+            {
+              data: {
+                name,
+                email
+              }
+            }),
+            q.Get(
+              q.Match(q.Index('user_by_email'),q.Casefold(user.email))
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    }
+  }
 })
