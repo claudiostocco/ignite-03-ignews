76ab8316ec0b1a3b62a256d5877a525e30a0b676
developer 203 <claudiostocco@gmail.com>
2021-07-14 10:55:49 -0300

    Adding revalidate parameter in getStaticProps at preview.

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useSession } from 'next-auth/client'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -52,7 +52,7 @@ export default function PreviewPost({ post }: PreviewPostProps) {
     )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
         fallback: 'blocking'
@@ -78,6 +78,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     return {
         props: {
             post
-        }
+        },
+        revalidate: 30 * 60 // 30 Minutes
     }
 }
